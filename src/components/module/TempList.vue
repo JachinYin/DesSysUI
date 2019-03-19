7c7eccdc1ec40b4a2fba7e16251881be35c61d95
JachinYin <1106025235@qq.com>
2019-03-19 23:28:49 +0800

    统一跳转请求URL

--- src/components/module/TempList.vue
+++ src/components/module/TempList.vue
@@ -116,7 +116,7 @@
         let thiz = this;
         let tempId = thiz.templateList[index].tempId;
         $.ajax({
-          url: thiz.preUrl + "getTempById",
+          url: thiz.preUrl + "getTempAuditById",
           data: {
             tempId: tempId,
           },
