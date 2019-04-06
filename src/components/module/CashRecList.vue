24b34d4ad7ee18750b328404d9a0be8dee4538d6
JachinYin <1106025235@qq.com>
2019-04-06 22:18:30 +0800

    提现记录简单化

--- src/components/module/CashRecList.vue
+++ src/components/module/CashRecList.vue
@@ -6,17 +6,17 @@
     </div>
     <div class="table">
       <el-table :data="cashRecList.slice(index,size)" border :height="609" tooltip-effect="light">
-        <el-table-column align="center" label="提交时间" prop="time" width="200px"></el-table-column>
-        <el-table-column align="center" label="AID" prop="aid" width="170px"></el-table-column>
+        <el-table-column align="center" label="提现时间" prop="time" width="200px"></el-table-column>
+        <el-table-column align="center" label="AID" prop="aid" width="280px"></el-table-column>
         <el-table-column align="center" label="设计师名称" prop="nickName" width="200px"
                          show-overflow-tooltip></el-table-column>
         <el-table-column align="center" label="本次提现金额" prop="price" width="200px"></el-table-column>
         <el-table-column align="center" label="剩余可提现金额" prop="balance" width="200px"></el-table-column>
-        <el-table-column align="center" label="操作" width="125px">
-          <template slot-scope="scope">
-            <span @click="showCashDetailBox(scope.$index)" class="showDetail_btn">查看</span>
-          </template>
-        </el-table-column>
+        <!--<el-table-column align="center" label="操作" width="125px">-->
+          <!--<template slot-scope="scope">-->
+            <!--<span @click="showCashDetailBox(scope.$index)" class="showDetail_btn">查看</span>-->
+          <!--</template>-->
+        <!--</el-table-column>-->
       </el-table>
     </div>
     <div class="pageNav">
